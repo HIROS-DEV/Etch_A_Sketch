7285481cb6640ad8a91f00e0aa1c0407c2822ccf
Hiroyuki Koda <95255730+HIROS-DEV@users.noreply.github.com>
2023-05-10 12:25:33 +0900

    Add hover effect with using Javascript

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const gridContainer = document.querySelector('.grid-container');
 
 for (let i = 0; i < 256; i++) {
-	const div = document.createElement('div');
-	div.style.border = '1px solid black';
+    const div = document.createElement('div');
+    div.classList.add('grid-item')
 	gridContainer.appendChild(div);
 }
+
+const gridItems = document.querySelectorAll('.grid-item');
+gridItems.forEach(grid => {
+    grid.addEventListener('mouseenter', ()=> {
+        grid.style.backgroundColor = "yellow";
+    })
+})
