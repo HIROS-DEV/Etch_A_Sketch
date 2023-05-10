63207e5c7dd7a5dba96c3896fccb848a6186da3a
Hiroyuki Koda <95255730+HIROS-DEV@users.noreply.github.com>
2023-05-11 08:19:05 +0900

    Finish etch a sketch project

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const gridContainer = document.querySelector('.grid-container');
+const selectBtn = document.querySelector('button');
 
 for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid-item')
+	const div = document.createElement('div');
+	div.classList.add('grid-item');
 	gridContainer.appendChild(div);
 }
 
 const gridItems = document.querySelectorAll('.grid-item');
-gridItems.forEach(grid => {
-    grid.addEventListener('mouseenter', ()=> {
-        grid.style.backgroundColor = "yellow";
-    })
-})
+
+gridItems.forEach((grid) => {
+    grid.addEventListener('mouseenter', () => {
+        grid.style.backgroundColor = 'yellow';
+    });
+});
+
+selectBtn.addEventListener('click', (e) => {
+	gridContainer.replaceChildren();
+    const gridNumber = prompt('Please enter a grid number', 16);
+    
+    if (gridNumber > 100) return;
+    if (gridNumber < 0) return;
+
+	for (let i = 0; i < gridNumber * gridNumber; i++) {
+		const div = document.createElement('div');
+		div.classList.add('grid-item');
+		gridContainer.style.gridTemplateColumns = `repeat(${gridNumber},1fr)`;
+		gridContainer.appendChild(div);
+    
+    
+    const gridItems = document.querySelectorAll('.grid-item');
+
+		gridItems.forEach((grid) => {
+			grid.addEventListener('mouseenter', () => {
+				grid.style.backgroundColor = 'yellow';
+			});
+		});}
+});
